b3b78b802166b238a8d11da56e063986e9c54d00
tenus88 <diachenkoandrei1@gmail.com>
2020-08-01 11:56:39 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,5 +1,6 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+        System.out.println("I love you all");
     }
 }
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,6 +2,7 @@ public class HelloB20 {
 
     public static void main(String[] args) {
         System.out.println("Hello B20, it is my pleasure to be with you!");
+
     }
 
 
