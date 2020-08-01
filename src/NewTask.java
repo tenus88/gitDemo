c8986be0eda6b8281a6a905696fc7a32a66ab891
tenus88 <diachenkoandrei1@gmail.com>
2020-08-01 14:08:25 -0400

    add New Task2

--- src/NewTask.java
+++ src/NewTask.java
@@ -2,5 +2,9 @@ public class NewTask {
     public static void main(String[] args) {
         System.out.println("I love Java");
         System.out.println("I love my mom");
+
+
+
+        //somelcn
     }
 }
---
+++ src/NewTask2.java
@@ -0,0 +1,5 @@
+public class NewTask2 {
+    public static void main(String[] args) {
+        String str = "One more task";
+    }
+}
