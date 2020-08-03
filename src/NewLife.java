67ef446466d83f53b427a508712e4f507639d5cb
tenus88 <diachenkoandrei1@gmail.com>
2020-08-03 12:33:24 -0400

    Ka-ka

--- src/NewLife.java
+++ src/NewLife.java
@@ -3,6 +3,7 @@ public class NewLife {
     public static void main(String[] args) {
  
         System.out.println ("I wanna milk");
-        System.out.println( "I wanna be a rich");
+        System.out.println( "I wanna be rich");
+        // aqwe
     }
 }
