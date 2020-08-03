00aa996a301217393130aee6fdd24160afe839f0
tenus88 <63273534+tenus88@users.noreply.github.com>
2020-08-03 12:28:21 -0400

    Update NewLife.java

--- src/NewLife.java
+++ src/NewLife.java
@@ -1,8 +1,8 @@
 public class NewLife {
 
     public static void main(String[] args) {
-
-
+ 
+        System.out.println ("I wanna milk");
         System.out.println( "I wanna be a rich");
     }
 }
